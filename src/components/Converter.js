5017a366e3a727c6a0a0f266905e1a4a6ca23bf8
MHValdez <marcos.h.valdez@gmail.com>
2023-03-12 23:48:02 -0500

    Implemented significant figures

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -6,22 +6,122 @@ import Dropdown from '../components/Dropdown';
 
 import { units } from "../components/Units";
 
+function sigFigs(magnitude) {
+    const input_length = magnitude.length;
+    let sig_figs = input_length;
+
+    let leading_zeroes = false;
+    let trailing_zeroes = false;
+    let decimal = false;
+
+    let num_leading = 0;
+    let num_trailing = 0;
+
+    let i = 0;
+
+    if (magnitude[0] == '0') {
+        leading_zeroes = true;
+    } 
+
+    while (leading_zeroes && i < input_length - 1) {
+        // Count leading zeroes
+        if (magnitude[i] == '0') {
+            sig_figs--;
+            num_leading++;
+            i++;
+        } else {
+            leading_zeroes = false;
+        }
+    }
+
+    while (i < input_length && decimal == false) {
+        // Check for decimals
+        if (magnitude[i] != '.') {
+            sig_figs--;
+            decimal = true;
+        }
+        i++;
+    }
+
+    i = input_length-1;
+
+    if (decimal == false && magnitude[i] == '0') {
+        trailing_zeroes = true;
+    }
+
+    while (trailing_zeroes && i > 0) {
+        // Count trailing zeroes for magnitudes without a decimal part
+        if (magnitude[i] == '0') {
+            sig_figs--;
+            num_trailing++;
+            i--;
+        } else {
+            trailing_zeroes = false;
+        }
+    }
+
+    return [sig_figs, num_leading, num_trailing];
+}
+
+function formatInput(magnitude, leading_zeroes) {
+    if (magnitude == '0') {
+        return magnitude
+    }
+    return magnitude.slice(leading_zeroes);
+}
+
+function formatOutput(magnitude, sig_figs) {
+    if (magnitude == '0') {
+        return magnitude
+    }
+
+    let formatted = magnitude.toString();
+    const in_length = formatted.length;
+    let out_length = sig_figs;
+    let decimal = undefined;
+    let i = 0;
+
+
+    while (decimal == undefined && i < in_length) {
+        if (formatted[i] == '.') {
+            decimal = i;
+        }
+        i++;
+    }
+
+    if (decimal != undefined && decimal < out_length) {
+        out_length++;
+    } else if (decimal > out_length) {
+        // Return all digits up to decimal
+        return formatted.slice(0, decimal);
+    }
+
+    // Return significant figures beyond decimal
+    return formatted.slice(0, out_length);
+}
+
 function Converter({ quantity }) {
-    const unit_dict = units[0][`${quantity}`]['units']
-    const unit_select = Object.keys(unit_dict)
-    const unit_type = units[0][`${quantity}`]['type']
-    const default_unit = unit_select[0];
-    const default_about = unit_dict[default_unit]['about_unit'];
-    const about_units = [default_about, default_about];
-    const abrv = [default_unit['abbreviation'], default_unit['abbreviation']]
-    const sys = [default_unit['system'], default_unit['system']]
+    // Extract info on query quantity from Units library
+    const unit_dict = units[0][`${quantity}`]['units'];
+    const unit_type = units[0][`${quantity}`]['type'];
     const about_qty = units[0][`${quantity}`]['about_qty'];
 
+    const unit_select = Object.keys(unit_dict);
+    const default_unit = unit_select[0];
+
+    const default_about = unit_dict[default_unit]['about_unit'];
+    const about_units = [default_about, default_about];
+
+    const abrv = [default_unit['abbreviation'], default_unit['abbreviation']];
+    const sys = [default_unit['system'], default_unit['system']];
+    
     const [magnitude, setMagnitude] = useState('0');
     const [unit_from, setUnitFrom]  = useState(default_unit);
     const [unit_to, setUnitTo]      = useState(default_unit);
 
+    // Even handlers
     const handleSubmit = (event) => {
+        // Query microservice and display results
         event.preventDefault();
 
         const input = {
@@ -32,6 +132,10 @@ function Converter({ quantity }) {
             unit_to: unit_to
         };
 
+        const sig_fig_data = sigFigs(magnitude);
+        const sig_figs = sig_fig_data[0];
+        const leading_zeroes = sig_fig_data[1];
+
         const request = JSON.stringify(input);
 
         console.log(`Request: ${request}`);
@@ -41,12 +145,20 @@ function Converter({ quantity }) {
             headers: {'Content-Type': 'application/json',},
             body: request
         }).then((response) => response.json()
-        ).then((data) => alert(
-            `Result: ${magnitude} ${unit_from} = ${data.magnitude} ${unit_to}\n\nConversion Factor: ${data.factor}`
-            ));
+        ).then((data) => {
+            const sig_input = formatInput(magnitude, leading_zeroes);
+            const sig_output = formatOutput(data.magnitude, sig_figs);
+            const sig_factor = formatOutput(data.factor, sig_figs);
+
+            const result = `Result: ${sig_input} ${unit_from} = ${sig_output} ${unit_to}`;
+            const factor = `\n\nConversion Factor: ${sig_factor}`;
+
+            alert(result + factor);
+        });
     }
 
     const showUnitInfo = (event) => {
+        // Display explanatory dialog
         event.preventDefault();
 
         about_units[0] = unit_dict[unit_from]['about_unit'];
@@ -65,11 +177,13 @@ function Converter({ quantity }) {
     }
 
     const showQtyInfo = (event) => {
+        // Display explanatory dialog
         event.preventDefault();
 
         alert(about_qty);
     }
 
+    // Form body
     return (
         <form onSubmit={handleSubmit}>
 
--- src/components/Systems.js
+++ src/components/Systems.js
@@ -3,9 +3,9 @@ export const systemInfo = [
     "International System of Units (SI)":
     "The most highly standardized and widely used system of units. Derived from the traditional metric system.",
     "Imperial System of Units (BI)":
-    "Used alongside SI in the UK. Derived traditional Eglish units. Large overlap with USC.",
+    "Used alongside SI in the UK. Derived from traditional Eglish units. Large overlap with USC.",
     "United States Customary Units (USC)":
-    "Used alongside SI in the US. Derived traditional Eglish units. Large overlap with BI.",
+    "Used alongside SI in the US. Derived from traditional Eglish units. Large overlap with BI.",
     "Other Systems":
     "There are many non-standard units in traditional English and metric systems as well as other archaic systems that are not supported by this converter. Some units that are not part of the major systems are in common use and are supported. Their systems are listed as NA."
 }
